cc3296ecd397134b9674ec8391148dadfb59a856
Scott Cordle <scordle@sullivan.edu>
2019-08-29 15:09:07 -0400

    new form is working

--- public/create.php
+++ public/create.php
@@ -18,14 +18,14 @@ if (isset($_POST['submit'])) {
     $new_user = array(
       "firstname" => $_POST['firstname'],
       "lastname"  => $_POST['lastname'],
-      "email"     => $_POST['email'],
-      "age"       => $_POST['age'],
+      // "email"     => $_POST['email'],
+      // "age"       => $_POST['age'],
       "location"  => $_POST['location']
     );
 
     $sql = sprintf(
       "INSERT INTO %s (%s) values (%s)",
-      "users",
+      "person",
       implode(", ", array_keys($new_user)),
       ":" . implode(", :", array_keys($new_user))
     );
@@ -51,10 +51,10 @@ if (isset($_POST['submit'])) {
     <input type="text" name="firstname" id="firstname">
     <label for="lastname">Last Name</label>
     <input type="text" name="lastname" id="lastname">
-    <label for="email">Email Address</label>
+    <!-- <label for="email">Email Address</label>
     <input type="text" name="email" id="email">
     <label for="age">Age</label>
-    <input type="text" name="age" id="age">
+    <input type="text" name="age" id="age"> -->
     <label for="location">Location</label>
     <input type="text" name="location" id="location">
     <input type="submit" name="submit" value="Submit">
